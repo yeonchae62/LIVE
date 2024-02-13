19af58aa6017e7f1b46587110b4308767d7bdeef
yeonchae62@tamu.edu <yeonchae62@tamu.edu>
2024-02-13 17:13:47 -0600

    acceptance testing

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  get '/my_info', to: 'main#my_info'
+  get '/categories', to: 'main#categories'
+  get '/faq', to: 'main#faq'
 
   # Defines the root path route ("/")
   # root "posts#index"
