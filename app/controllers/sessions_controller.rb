b8095d9c5495bb3394e9ffed547da4fa410a8d75
yeonchae62 <yeonchae62@tamu.edu>
2024-02-14 14:58:30 -0600

    login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,15 @@
-Class SessionsController < ApplicationController
-
+class SessionsController < ApplicationController
     def create
-        @user = User.find_by(username: params[:username])
-
-        if !@user && @user.authenticate(params[:password])
-            session[:user_id] = @user.id
-            redirect_to user_path
-
-        else
-            
+      @user = User.find_by(username: params[:username])
+  
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        # Error message on failure
+        message = "Invalid username or password"
+        redirect_to login_path, notice: message
+      end
+    end
+  end
+  
