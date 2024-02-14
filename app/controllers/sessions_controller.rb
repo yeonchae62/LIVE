10e6cba5095a6300f6995276454dce93b0778375
yeonchae62 <yeonchae62@tamu.edu>
2024-02-14 13:48:30 -0600

    login add

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,11 @@
+Class SessionsController < ApplicationController
+
+    def create
+        @user = User.find_by(username: params[:username])
+
+        if !@user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to user_path
+
+        else
+            
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'main#index'
   resources :games
 
+  resources :users, only: [:new, :create, :update, :destroy, :edit, :show]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,6 +12,11 @@ Rails.application.routes.draw do
   get '/my_info', to: 'main#my_info'
   get '/categories', to: 'main#categories'
   get '/faq', to: 'main#faq'
+
+  get '/login', to: 'main#login'
+  get 'login', to: 'main#create'
+  get '/logout', to: 'main#logout'
+  get '/logout', to: 'main#destroy'
 
   # Defines the root path route ("/")
   # root "posts#index"
