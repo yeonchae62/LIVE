546ef8ae91f9e1be52970ef72cd231ec878183d2
yeonchae62 <yeonchae62@tamu.edu>
2024-02-16 05:24:13 -0600

    main page sync with main branch and make a login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/login', to: 'sessions#new'
+  
   root 'main#index'
   resources :games
 
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  get "/login", to: "main#login"
 
   # Defines the root path route ("/")
   # root "posts#index"
