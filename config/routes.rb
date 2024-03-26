1be32fbcdb8bbd91e295139b7f17aadb834d8061
Zach <cholmcdowell@gmail.com>
2024-03-26 17:49:00 -0500

    user_management page

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
   get '/login', to: 'main#login'
+  get '/user_management', to: 'main#user_management'
+
   get '/', to: 'main#index', as: :main_page
 
   # Defines the root path route ("/")
