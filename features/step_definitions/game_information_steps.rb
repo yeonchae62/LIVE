9e5869d19f6e6e84c8daf64aeb420c16656f28e8
xiuyu-lana <xiuyu@tamu.edu>
2024-03-27 17:10:52 -0500

    solve some rubocop offenses

--- features/step_definitions/game_information_steps.rb
+++ features/step_definitions/game_information_steps.rb
@@ -2,18 +2,17 @@
 
 Given('I have a game {string} with the image_url {string}') do |game_title, image_url|
   # Create a game with the given title and image_url
-  Game.create!(game_title: game_title, image: image_url)
+  Game.create!(game_title:, image: image_url)
 end
 
 Given('I have a game {string} without an image') do |game_title|
   # Create a game with the given title and no image_url
-  Game.create!(game_title: game_title)
+  Game.create!(game_title:)
 end
-
 
 When('I visit the game information page for {string}') do |game_title|
   # Visit the page that shows the information of the specified game
-  game = Game.find_by(game_title: game_title)
+  game = Game.find_by(game_title:)
   visit game_path(game)
 end
 
