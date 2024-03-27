37f3bee48c38636f3cfb22b53c9bfb46f63929a6
yeonchae62 <yeonchae62.tamu.edu>
2024-03-27 00:28:06 -0500

    rubocop

--- features/step_definitions/game_information_steps.rb
+++ features/step_definitions/game_information_steps.rb
@@ -1,25 +1,28 @@
-Given("I have a game {string} with the image_url {string}") do |game_title, image_url|
+# frozen_string_literal: true
+
+Given('I have a game {string} with the image_url {string}') do |game_title, image_url|
   # Create a game with the given title and image_url
   Game.create!(game_title: game_title, image: image_url)
 end
 
-When("I visit the game information page for {string}") do |game_title|
+Given('I have a game {string} without an image') do |game_title|
+  # Create a game with the given title and no image_url
+  Game.create!(game_title: game_title)
+end
+
+
+When('I visit the game information page for {string}') do |game_title|
   # Visit the page that shows the information of the specified game
   game = Game.find_by(game_title: game_title)
   visit game_path(game)
 end
 
-Given("I have a game {string} without an image") do |game_title|
-  # Create a game with the given title and without an image
-  Game.create!(game_title: game_title)
-end
-
-Then("I should see an image for the game") do
+Then('I should see an image for the game') do
   # Verify that an image is present for the game
   expect(page).to have_css('img')
 end
 
-Then("I should not see an image for the game") do
+Then('I should not see an image for the game') do
   # Verify that no image is present for the game
   expect(page).to have_css('img')
 end
--- features/step_definitions/game_list_with_image_steps.rb
+++ features/step_definitions/game_list_with_image_steps.rb
@@ -1,26 +1,27 @@
-Given("there are games with images") do
-    # Create games with images
-    Game.create!(game_title: "Game1", url: "http://example.com", image: "http://example.com/image1.jpg", platform: "Platform1")
-    Game.create!(game_title: "Game2", url: "http://example.com", image: "http://example.com/image2.jpg", platform: "Platform2")
-  end
-  
-  Given("there are games without images") do
-    # Create games without images
-    Game.create!(game_title: "Game3", url: "http://example.com", platform: "Platform3")
-    Game.create!(game_title: "Game4", url: "http://example.com", platform: "Platform4")
-  end
-  
-  When("I visit the games list page") do
-    visit games_path
-  end
-  
-  Then("I should see game information with images") do
-    # Verify that game information with images is displayed
-    expect(page).to have_css('.game .game-info .game-image')
-  end
-  
-  Then("I should see game information without images") do
-    # Verify that game information without images is displayed
-    expect(page).to have_css('.game .game-info .game-image')
-  end
-  
+# frozen_string_literal: true
+
+Given('there are games with images') do
+  # Create games with images
+  Game.create!(game_title: 'Game1', url: 'http://example.com', image: 'http://example.com/image1.jpg')
+  Game.create!(game_title: 'Game2', url: 'http://example.com', image: 'http://example.com/image2.jpg')
+end
+
+Given('there are games without images') do
+  # Create games without images
+  Game.create!(game_title: 'Game3', url: 'http://example.com')
+  Game.create!(game_title: 'Game4', url: 'http://example.com')
+end
+
+When('I visit the games list page') do
+  visit games_path
+end
+
+Then('I should see game information with images') do
+  # Verify that game information with images is displayed
+  expect(page).to have_css('.game .game-info .game-image')
+end
+
+Then('I should see game information without images') do
+  # Verify that game information without images is displayed
+  expect(page).to have_css('.game .game-info .game-image')
+end
