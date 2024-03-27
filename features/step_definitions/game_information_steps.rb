e4360c231f18c51181b4ff8fe9091482d6ba3869
yeonchae62 <yeonchae62.tamu.edu>
2024-03-27 00:05:13 -0500

    add cucumber scenarios for the game-new page

--- features/step_definitions/game_information_steps.rb
+++ features/step_definitions/game_information_steps.rb
@@ -1,25 +1,25 @@
-Given("I have a game {string} with the image_url {string}") do |game_title, image|
-    # Create a game with the given title and image_url
-    Game.create!(game_title: game_title, image: image)
-  end
-  
-  
-  When("I visit the game information page for {string}") do |game_title|
-    # Visit the page that shows the information of the specified game
-    game = Game.find_by(game_title: game_title)
-    visit game_path(game)
-  end
-  
-  Then("I should see an image for the game") do
-    # Verify that an image is present for the game
-    expect(page).to have_css('img')
-  end
-  
-  Then("I should see a link to edit the game") do
-    expect(page).to have_link('Edit this game')
-  end
-  
-  Then("I should see a link to go back to the games list") do
-    expect(page).to have_link('Back to games')
-  end
-  
+Given("I have a game {string} with the image_url {string}") do |game_title, image_url|
+  # Create a game with the given title and image_url
+  Game.create!(game_title: game_title, image: image_url)
+end
+
+When("I visit the game information page for {string}") do |game_title|
+  # Visit the page that shows the information of the specified game
+  game = Game.find_by(game_title: game_title)
+  visit game_path(game)
+end
+
+Given("I have a game {string} without an image") do |game_title|
+  # Create a game with the given title and without an image
+  Game.create!(game_title: game_title)
+end
+
+Then("I should see an image for the game") do
+  # Verify that an image is present for the game
+  expect(page).to have_css('img')
+end
+
+Then("I should not see an image for the game") do
+  # Verify that no image is present for the game
+  expect(page).to have_css('img')
+end
