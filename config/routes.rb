e5a95cc44e88540cf5c1e342a2f5d84e9148a2ce
xiuyu-lana <xiuyu@tamu.edu>
2024-04-03 11:20:58 -0500

    add my_profile page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   root 'main#index'
   resources :games
 
+  get '/users/my_profile', to: 'users#show', as: :user_account_info
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
