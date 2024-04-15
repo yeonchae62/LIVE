80db518f939d0f93000f519cec525e1552362036
xiuyu-lana <xiuyu@tamu.edu>
2024-04-14 23:00:06 -0500

    add authentication for user management page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class MainController < ApplicationController
+  before_action :authenticate_user!, only: %i[user_management change_role]
+  before_action :check_admin, only: %i[user_management change_role]
+
   def index
     @most_watched = Game.where(game_title: ['Lost Recipes', 'Roboco', 'Morning in Your Eyes',
                                             'Arté: Hemut', 'Variant: Limits', 'Arté: Mercenas'])
@@ -14,5 +17,13 @@ class MainController < ApplicationController
     user = User.find(params[:user_id])
     user.update(role: params[:role])
     redirect_to user_management_path, notice: 'User role updated successfully.'
+  end
+
+  private
+
+  def check_admin
+    return if current_user.admin?
+
+    redirect_to root_path, alert: 'You are not authorized to view this page.'
   end
 end
