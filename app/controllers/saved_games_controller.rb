34ade637a84c582d6446e76e726b0b7252b15157
yeonchae62 <yeonchae62.tamu.edu>
2024-04-19 23:00:22 -0500

    add all css contents with save function

--- app/controllers/saved_games_controller.rb
+++ app/controllers/saved_games_controller.rb
@@ -5,10 +5,17 @@ class SavedGamesController < ApplicationController
       @game = Game.find(params[:game_id])
       @saved_game = current_user.saved_games.build(game: @game)
   
-      if @saved_game.save
-        redirect_to games_path, notice: 'Game saved successfully!'
+        # Check if the game is already saved by the current user
+      if current_user.saved_games.where(game_id: @game.id).exists?
+        redirect_to games_path, notice: 'This game is already saved.'
       else
-        redirect_to games_path, alert: 'Failed to save the game.'
+        @saved_game = current_user.saved_games.build(game: @game)
+
+        if @saved_game.save
+          redirect_to games_path, notice: 'Game saved successfully!'
+        else
+          redirect_to games_path, alert: 'Failed to save the game.'
+        end
       end
     end
   end
