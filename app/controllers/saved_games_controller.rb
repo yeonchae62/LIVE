8c9c218bbd64368219d71c66b081285ea64ab220
yeonchae62 <yeonchae62.tamu.edu>
2024-04-20 02:20:37 -0500

    cucumber+rspec

--- app/controllers/saved_games_controller.rb
+++ app/controllers/saved_games_controller.rb
@@ -1,21 +1,24 @@
 class SavedGamesController < ApplicationController
-    before_action :authenticate_user!
-  
-    def create
-      @game = Game.find(params[:game_id])
-      @saved_game = current_user.saved_games.build(game: @game)
-  
-        # Check if the game is already saved by the current user
-      if current_user.saved_games.where(game_id: @game.id).exists?
-        redirect_to games_path, notice: 'This game is already saved.'
-      else
-        @saved_game = current_user.saved_games.build(game: @game)
+  before_action :authenticate_user!
 
-        if @saved_game.save
-          redirect_to games_path, notice: 'Game saved successfully!'
-        else
-          redirect_to games_path, alert: 'Failed to save the game.'
-        end
+  def index
+    @saved_games = current_user.saved_games
+  end
+
+  def create
+    @game = Game.find(params[:game_id])
+
+    # Check if the game is already saved by the current user
+    if current_user.saved_games.exists?(game_id: @game.id)
+      redirect_to games_path, notice: 'This game is already saved.'
+    else
+      @saved_game = current_user.saved_games.build(game: @game)
+
+      if @saved_game.save
+        redirect_to games_path, notice: 'Game saved successfully!'
+      else
+        redirect_to games_path, alert: 'Failed to save the game.'
       end
     end
   end
+end
--- spec/controllers/saved_games_controller_spec.rb
+++ spec/controllers/saved_games_controller_spec.rb
@@ -1,56 +1,53 @@
+# spec/controllers/saved_games_controller_spec.rb
 require 'rails_helper'
 
 RSpec.describe SavedGamesController, type: :controller do
-    describe "POST #create" do
-      context "when user is authenticated" do
-        before do
-          @user = User.create(email: "test@example.com", password: "password")
-          sign_in @user
-        end
-  
-        context "with valid params" do
-          let(:game) { Game.create(game_title: "Test Game") }
-  
-          it "creates a new saved game" do
-            expect {
-              post :create, params: { game_id: game.id }
-            }.to change(@user.saved_games, :count).by(1)
-          end
-  
-          it "redirects to games path with success notice" do
-            post :create, params: { game_id: game.id }
-            expect(response).to redirect_to(games_path)
-            expect(flash[:notice]).to eq('Game saved successfully!')
-          end
-        end
-  
-        context "when the game is already saved by the user" do
-          let(:game) { Game.create(game_title: "Test Game") }
-  
-          before do
-            @user.saved_games.create(game: game)
-          end
-  
-          it "does not create a new saved game" do
-            expect {
-              post :create, params: { game_id: game.id }
-            }.to_not change(@user.saved_games, :count)
-          end
-  
-          it "redirects to games path with notice" do
-            post :create, params: { game_id: game.id }
-            expect(response).to redirect_to(games_path)
-            expect(flash[:notice]).to eq('This game is already saved.')
-          end
-        end
+  describe "POST #create" do
+    context "when the game is not already saved" do
+      let(:user) { User.create!(email: 'test@example.com', password: 'password1234', role: 0) }
+      let!(:game) { Game.create!(game_title: 'Game1', url: 'http://game1.com') }
+
+      it "creates a saved game" do
+        sign_in user
+        post :create, params: { game_id: game.id }
+        expect(SavedGame.count).to eq(1)
       end
-  
-      context "when user is not authenticated" do
-        it "redirects to sign in page" do
-          post :create
-          expect(response).to redirect_to(new_user_session_path)
-        end
+
+      it "redirects to the games index page" do
+        sign_in user
+        post :create, params: { game_id: game.id }
+        expect(response).to redirect_to(games_path)
+      end
+
+      it "sets a success notice" do
+        sign_in user
+        post :create, params: { game_id: game.id }
+        expect(flash[:notice]).to eq("Game saved successfully!")
+      end
+    end
+
+    context "when the game is already saved" do
+      let(:user) { User.create!(email: 'test@example.com', password: 'password1234', role: 0) }
+      let!(:game) { Game.create!(game_title: 'Game1', url: 'http://game1.com') }
+      let!(:saved_game) { SavedGame.create(user: user, game: game) }
+
+      it "does not create a duplicate saved game" do
+        sign_in user
+        post :create, params: { game_id: game.id }
+        expect(SavedGame.count).to eq(1)
+      end
+
+      it "redirects to the games index page" do
+        sign_in user
+        post :create, params: { game_id: game.id }
+        expect(response).to redirect_to(games_path)
+      end
+
+      it "sets a notice indicating the game is already saved" do
+        sign_in user
+        post :create, params: { game_id: game.id }
+        expect(flash[:notice]).to eq("This game is already saved.")
       end
     end
   end
-  
+end
