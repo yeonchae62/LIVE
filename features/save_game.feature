305cee322c3401cae733898b46062f6cebd97e22
yeonchae62 <yeonchae62.tamu.edu>
2024-04-20 01:49:08 -0500

    cucumber

--- features/save_game.feature
+++ features/save_game.feature
@@ -1,11 +1,20 @@
-Feature: Save game functionality
+Feature: Save Game
   As a logged-in user
-  I want to be able to save a game
-  So that I can access it later
+  I want to save games
+  So that I can access them later
 
-  Scenario: Save a game from the games index page
+  Scenario: Save a game successfully
     Given I am logged in to save a game
-    When I visit the games index page to save a game
-    And I click on the Save button for game "Test Game"
-    Then I should see a success message "Game saved successfully!"
-    And I should see the game "Test Game" in my saved games list
+    Given "game" is in the index page
+    And I have not already saved the game "game"
+    And I visit the games index page to save a game
+    When I click on the Save button for the game "game"
+    Then I should see that the game saved successfully!
+
+  Scenario: Save an already saved game
+    Given I am logged in to save a game
+    Given "game" is in the index page
+    And I have already saved the game "game"
+    And I visit the games index page to save a game
+    When I click on the Save button for the game "game"
+    Then I should see this game is already saved
--- features/step_definitions/save_game_steps.rb
+++ features/step_definitions/save_game_steps.rb
@@ -1,28 +1,43 @@
 Given("I am logged in to save a game") do
-    @user = User.create(email: "test@example.com", password: "password")
-    visit new_user_session_path
-    fill_in "Email", with: "test@example.com"
-    fill_in "Password", with: "password"
-    click_button "Log in"
-  end
-  
-  When("I visit the games index page to save a game") do
-    visit games_path
-  end
-  
-  And("I click on the Save button for game {string}") do |game_title|
-    game = Game.find_by(game_title: game_title)
-    within('.games-info') do
-      click_link("Save", href: saved_games_path(game_id: game.id))
-    end
-  end
-  
-  Then("I should see a success message {string}") do |message|
-    expect(page).to have_content message
-  end
-  
-  And("I should see the game in my saved games list") do
-    visit saved_games_path
-    expect(page).to have_content(game_title)
-  end
-  
+  @user = User.create(email: "test@example.com", password: "password")
+  visit new_user_session_path
+  fill_in "Email", with: "test@example.com"
+  fill_in "Password", with: "password"
+  click_button "Log in"
+end
+
+And("I visit the games index page to save a game") do
+  visit games_path
+end
+
+Given("{string} is in the index page") do |game_title|
+  # Assuming the game "Game" is displayed on the index page
+  # You may need to customize this step according to your application
+  Game.create(game_title: game_title)
+end
+
+And("I have not already saved the game {string}") do |game_title|
+  # No specific action needed as we assume the game is not already saved
+  game = Game.find_by(game_title: game_title)
+  user = User.first
+  saved_game = SavedGame.find_by(user: user, game: game)
+  expect(saved_game).to be_nil
+end
+
+And("I have already saved the game {string}") do |game_title|
+  game = Game.find_by(game_title: game_title)
+  user = User.first
+  SavedGame.create(user: user, game: game)
+end
+
+When("I click on the Save button for the game {string}") do |game_title|
+  click_button("Save")
+end
+
+Then("I should see that the game saved successfully!") do
+  expect(page).to have_content("Game saved successfully!")
+end
+
+Then("I should see this game is already saved") do
+  expect(page).to have_content("This game is already saved.")
+end
